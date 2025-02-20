42c0877fa29f612ae0a55d10e3169e711dcf63aa
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-02-20 17:42:05 -0600

    login feature and step with page  object methods

--- src/test/java/stepDefinitions/LoginStep.java
+++ src/test/java/stepDefinitions/LoginStep.java
@@ -1,5 +1,189 @@
 package stepDefinitions;
 
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+
+import org.testng.Assert;
+
+import driverFactory.DriverFactory;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pageObjectRepository.LoginPage;
+
 public class LoginStep {
+	
+	private LoginPage login = new LoginPage(DriverFactory.getDriver());
+	
+	private String title;
+	
+	@Given("The browser is open")
+	public void the_browser_is_open() {
+		
+		DriverFactory.getDriver();
+	}
+
+	@Then("Admin should land on the login page")
+	public void admin_should_land_on_the_login_page() {
+		
+		 title = login.getpagetitle();
+		Assert.assertEquals(title, "LMS");
+	   
+	}
+
+	@When("Admin gives the invalid LMS portal URL")
+	public void admin_gives_the_invalid_lms_portal_url() {
+		
+		login.invalidurl();
+	}
+
+	@Then("Admin should receive application error")
+	public void admin_should_receive_application_error() {
+		
+	    title = login.getinvalidurlalert();
+	    Assert.assertEquals(title, "There's nothing here, yet.");
+		
+	}
+
+	@Then("HTTP response >= {int}. then the link is broken")
+	public void http_response_then_the_link_is_broken(Integer code) throws IOException {
+		
+	   int responseCode =	login.getBrokenLinks();
+	
+		Assert.assertEquals("Link Response code is: " + responseCode, responseCode >= code);
+   
+	}
+
+	@Then("Admin should see correct spellings in all fields")
+	public void admin_should_see_correct_spellings_in_all_fields() {
+		
+		String userlabel = login.getusernamelabel();
+		  Assert.assertEquals(userlabel, "User");
+		  
+		  String Passwordlabel = login.getPasswordlabel();
+		  Assert.assertEquals(Passwordlabel, "Password");
+	}
+
+	@Then("Admin should see  LMS - Learning Management System")
+	public void admin_should_see_lms_learning_management_system() throws Exception {
+		
+		 String extractedText = login.applicationText();
+	     String expectedText = "LMS - Learning Management System";
+      
+
+        // Assert that the extracted text matches the expected text
+		  Assert.assertEquals(extractedText, expectedText);
+
+	}
+
+	@Then("Admin should see company name below the app name")
+	public void admin_should_see_company_name_below_the_app_name() throws Exception {
+		
+		   String companyname = login.getcomapnyname();
+            // Define the expected company name
+            String expectedText = "NumpyNinja"; 
+
+            Assert.assertEquals(companyname, expectedText);
+		
+	}
+
+	@Then("Admin should see {string}")
+	public void admin_should_see(String header) {
+		
+		title = login.getLoginHeader();
+		 Assert.assertEquals(title, header);
+		
+	}
+
+	@Then("Admin should see two text field")
+	public void admin_should_see_two_text_field() {
+		
+		Assert.assertTrue(login.userfieldspresent());
+		Assert.assertTrue(login.passwordfieldpresent());
+
+	}
+
+	@Then("Admin should {string} in the first text field")
+	public void admin_should_in_the_first_text_field(String string) {
+		
+		String userlabel = login.getusernamelabel();
+		  Assert.assertEquals(userlabel, string);
+	}
+
+	@Then("Admin should see asterisk mark symbol next to text for mandatory fields")
+	public void admin_should_see_asterisk_mark_symbol_next_to_text_for_mandatory_fields() {
+		
+		String text = login.userreqired();
+		Assert.assertEquals(text, "*");
+	}
+
+	@Then("Admin should {string} in the second text field")
+	public void admin_should_in_the_second_text_field(String string) {
+		
+		 String Passwordlabel = login.getPasswordlabel();
+		  Assert.assertEquals(Passwordlabel, string);
+	}
+
+	@Then("Admin should see asterisk mark symbol next to password text")
+	public void admin_should_see_asterisk_mark_symbol_next_to_password_text() {
+		
+		String text = login.passwordrequired();
+		Assert.assertEquals(text, "*");
+	}
+
+	@Then("Admin should see one dropdown")
+	public void admin_should_see_one_dropdown() {
+		
+		Assert.assertTrue(login.Dropdowndisplay());
+	}
+
+	@Then("Admin should see {string} placeholder in dropdown")
+	public void admin_should_see_placeholder_in_dropdown(String string) {
+		
+		String text = login.dropdownLabel();
+		Assert.assertEquals(text, string);
+	}
+
+	 @Then("Admin should see the following options in the dropdown:")
+	    public void admin_should_see_the_following_options_in_the_dropdown(DataTable dataTable) {
+		
+		 List<String> expectedOptions = dataTable.asList(String.class);
+		 List<String> actualDropdownValues = login.dropdownitems();
+			Assert.assertEquals(expectedOptions, actualDropdownValues);
+	}
+
+	@Then("Admin should see input field on the centre of the page")
+	public void admin_should_see_input_field_on_the_centre_of_the_page() {
+		
+		Assert.assertTrue(login.inputfields_centeralignment());
+	}
+
+	@Then("Admin should see login button")
+	public void admin_should_see_login_button() {
+	   
+		Assert.assertTrue(login.loginbutton());
+	}
+
+	@Then("Admin should see user in gray color")
+	public void admin_should_see_user_in_gray_color() {
+		
+		String color = login.getuserfieldcolor();
+		Assert.assertEquals(color, "rgba(128, 128, 128, 1)");
+	   
+	}
+
+	@Then("Admin should see password in gray color")
+	public void admin_should_see_password_in_gray_color() {
+		
+		String color = login.getPasswordcolor();
+		Assert.assertEquals(color, "rgba(128, 128, 128, 1)");
+	}
 
 }
