5590d9414dfa4a348de0bf9952f16a92336b24bb
sadhna12 <sadhna.upy@gmail.com>
2025-02-23 17:17:08 +0530

    added home page

--- src/main/java/pageObjectRepository/HomePage.java
+++ src/main/java/pageObjectRepository/HomePage.java
@@ -1,5 +1,384 @@
 package pageObjectRepository;
 
-public class HomePage {
+import static org.testng.Assert.assertEquals;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+
+public class HomePage  {
+  
+	WebDriver driver;//=new ChromeDriver();
+  @FindBy(xpath="//*[text()=\" LMS - Learning Management System \"]")
+  private WebElement Lmstitle;
+  @FindBy(xpath="//*[@class=\"mat-ripple mat-button-ripple\"]")
+  private WebElement alltextAlighnment;
+  
+  
+  
+
+  
+  @FindBy(xpath="//app-doughnutchart")
+  private WebElement piechart;
+  
+  @FindBy(xpath="//mat-card-content[@class='mat-card-content']//canvas[@class='chartjs-render-monitor']")
+  private WebElement barchart;
+  
+
+  @FindBy (xpath="//*[text()=\" Welcome sdetnumpyninja@gmail.com\"]")
+  WebElement welcomeMessage;
+  @FindBy(xpath="//*[text()=\"ADMIN\"]")
+  WebElement role;
+  
+  @FindBy (xpath="//*[@class=\"widget-container\"]")
+  WebElement count;
+
+  @FindBy (xpath="//*[@class=\"bottom\"]")
+  WebElement bottom;
+  
+ // @FindBy (xpath="(//div[@class='top'])[2]")
+ // WebElement top;
+  @FindBy (xpath="//*[@class=\"widget green\"]")
+  WebElement user;
+  @FindBy (xpath="//*[@aria-label=\"Previous page\"]")
+  WebElement previous;
+  
+  @FindBy (xpath="//*[@aria-label=\"First page\"]")
+  WebElement firstpage;
+  
+  @FindBy (xpath="//*[@aria-label=\"Next page\"]")
+  WebElement nextpage;
+  
+  @FindBy (xpath="//*[@aria-label=\"Last page\"]")
+  WebElement lastpage;
+  
+//  @FindBy (xpath="class=["mat-row cdk-row ng-star-inserted")
+//  WebElement five_staffdata;
+ 
+ 
+
+  public HomePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver,this);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		 driver = new ChromeDriver(options);
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+  
+
+ 
+ 
+  
+  //@FindBy(xpath="//*[text()=\" LMS - Learning Management System \"]")
+  //private WebElement Lmstitle;
+  
+  
+  
+
+public void landOnloginpage()
+  {
+	  
+	  //DriverFactory.initializeBrowser
+	  driver.get("https://feb-ui-hackathon-bbfd38d67ea9.herokuapp.com/login");
+  }
+  
+  public void login()
+  {
+	  driver.findElement(By.id("username")).sendKeys("sdetnumpyninja@gmail.com");
+	  driver.findElement(By.id("password")).sendKeys("Feb@2025");
+	  driver.findElement(By.xpath("//span[text()=\"Select the role\"]")).click();
+	  driver.findElement(By.xpath("//*[text()=\" Admin \"]")).click();
+	  driver.findElement(By.xpath("//*[text()=\"Login\"]")).click();
+	//assert.equals(driver.getTitle(),"LMS");
+	
+
+	  
+	  
+  }
+
+public void validateLmsTitle() {
+	
+	
+	System.out.println("title   "+driver.getTitle());
+	assertEquals(driver.getTitle(),"LMS");
+	
+	}
+
+public void verifyTitlePosition() 
+{
+	int xCoordinate=Lmstitle.getLocation().getX();
+	int yCoordinate=Lmstitle.getLocation().getY();
+	
+	System.out.println("xcordinate "+xCoordinate);
+	System.out.println("ycordinate "+yCoordinate);
+	int tolerance = 10;
+	
+	 Assert.assertEquals(true, xCoordinate >= tolerance && yCoordinate >= tolerance);
 }
+
+public void verifyNevigationbarTextSpelling() {
+	// TODO Auto-generated method stub
+	
+}
+
+public void veryfyTitleSpelling() {
+	String str=" LMS - Learning Management System ";
+	Assert.assertEquals(Lmstitle.getText().toString(), str);
+	
+}
+
+public void check_AlignmentOfTextField() {
+	
+	//LoggerLoad.info("Alignment input field is center position");
+	List<WebElement> texts=driver.findElements(By.xpath("//*[@class=\"mat-ripple mat-button-ripple\"]"));
+	
+	for(WebElement text_alighnment:texts)
+	{
+		String tex_Alignment= text_alighnment.getCssValue("text-align");
+		Assert.assertEquals("center", tex_Alignment);
+		System.out.println("alighnment  "+tex_Alignment);
+	}
+	
+	
+}
+
+public void checkTextPosition(String textname) {
+List<WebElement> texts=driver.findElements(By.xpath("//*[@class=\"mat-button-wrapper\"]"));
+   
+
+
+for (int i = 0; i < texts.size(); i++) {
+WebElement text = texts.get(i);
+    
+// Get the value (text) of the element
+String value = text.getText();
+    
+// Print the position (index) and value
+System.out.println("Position: " + i + " | Value: " + value);
+    
+//if (value.isEmpty()) {
+//    System.out.println("Element has no visible text.");
+//} else {
+//    System.out.println("Position: " + i + " | Value: " + value);
+//}
+//}
+//	for(WebElement text:texts)
+//	{
+		
+		
+		
+		if(textname.equalsIgnoreCase("Home"))
+		{
+			
+			//listItems.get(2).getText())
+			//System.out.println("alighnment  "+textname, texts.get(0).getText());
+			System.out.println("name  "+ texts.get(i).getText());
+			Assert.assertEquals(texts.get(i).getText(),textname);
+			break;
+		
+	//System.out.println("alighnment  "+tex_Alignment);
+	}
+		else if(textname.equalsIgnoreCase("Program"))
+		{
+			System.out.println("name    "+ texts.get(1).getText());
+			Assert.assertEquals(texts.get(i).getText(),textname);
+		break;
+		
+		//System.out.println("alighnment  "+tex_Alignment);
+	}
+		else if(textname.equalsIgnoreCase("Batch"))
+		{
+			System.out.println("name  "+ texts.get(i).getText());
+			Assert.assertEquals(texts.get(i).getText(),textname);
+		break;
+		
+		//System.out.println("alighnment  "+tex_Alignment);
+	}
+		else if(textname.equalsIgnoreCase("Class"))
+		{
+			System.out.println("name     "+ texts.get(i).getText());
+		Assert.assertEquals(textname, texts.get(i).getText());
+		break;
+		
+		
+	}
+		
+		else if(textname.equalsIgnoreCase("Logout"))
+		{
+			System.out.println("name     "+ texts.get(i).getText());
+		Assert.assertEquals(textname, texts.get(i).getText());
+		break;
+			
+	}
+	
+	}
+}
+
+public void pieChartisAvailable() {
+	boolean isavailable=piechart.isDisplayed();
+	Assert.assertEquals(true,isavailable);
+	
+}
+public void barChartisAvailable() {
+	boolean isavailable=barchart.isDisplayed();
+	Assert.assertEquals(true,isavailable);
+	
+}
+
+
+public void verifyEelcomeMessageAndRole() {
+	
+	
+	String user_role =role.getText().toString();
+	Assert.assertEquals(user_role,"ADMIN");
+	
+  String wel_msg=welcomeMessage.getText().toString();
+	
+	//boolean isWelcomemessageAvailable=welcomeMessage.isDisplayed();
+	Assert.assertEquals(wel_msg," Welcome sdetnumpyninja@gmail.com");
+	
+}
+  public void count(String value)
+  {
+	 // int i = 3;  // For example, you want to select the 3rd div
+	 
+	
+
+	  
+	// List<WebElement> count=driver.findElements(By.xpath("//*[@class=\"value\"]"));
+	 
+	 //String top=""//*[@class=\"widget green\"]"
+	 
+	/* for(int i=0;i<count.size();i++)
+	 {*/
+		 
+		 
+		 
+		 
+		  if(value.equalsIgnoreCase("User"))
+		  {
+			  String xpath = "(//div[@class='top'])[" + 2 + "]";
+			  WebElement top = driver.findElement(By.xpath(xpath));
+			  
+			  String bottom_xpath = "(//div[@class='bottom'])[" + 2 + "]";
+			   WebElement bottom = driver.findElement(By.xpath(bottom_xpath));
+			 
+		 String number=top.getText().toString();
+		 String valuename=bottom.getText().toString();
+		 
+		 System.out.println("number  =" +number    +"bottom value  =" + valuename);
+		 Assert.assertEquals(valuename, "User");
+		// break;
+		  }
+	// }
+		  
+		  else  if(value.equalsIgnoreCase("Staff"))
+		  {
+			  String xpath = "(//div[@class='top'])[" + 3 + "]";
+			  WebElement top = driver.findElement(By.xpath(xpath));
+			  String bottom_xpath = "(//div[@class='bottom'])[" + 3 + "]";
+			   WebElement bottom = driver.findElement(By.xpath(bottom_xpath));
+			   String number=top.getText().toString();
+				 String valuename=bottom.getText().toString();
+				 
+				 System.out.println("number  =" +number    +"bottom value  =" + valuename);
+				 Assert.assertEquals(valuename, "Staff");
+		 
+	 }
+	 
+		  else if(value.equalsIgnoreCase("Batches"))
+		  {
+			  String xpath = "(//div[@class='top'])[" + 4 + "]";
+			  WebElement top = driver.findElement(By.xpath(xpath));
+			  
+			  String number=top.getText().toString();
+				 String valuename=bottom.getText().toString();
+				 
+				 System.out.println("number  =" +number    +"bottom value  =" + valuename);
+				 Assert.assertEquals(valuename, "Batches");
+	 }
+		  else  if(value.equalsIgnoreCase("Programs"))
+		  {
+			  String bottom_xpath = "(//div[@class='bottom'])[" + 5 + "]";
+			   WebElement bottom = driver.findElement(By.xpath(bottom_xpath));
+			  String xpath = "(//div[@class='top'])[" + 5 + "]";
+			  WebElement top = driver.findElement(By.xpath(xpath));
+			 
+			  String number=top.getText().toString();
+				 String valuename=bottom.getText().toString();
+				 
+				 System.out.println("number  =" +number    +"bottom value  =" + valuename);
+				 Assert.assertEquals(valuename, "Programs");
+	 }
+	
+	 }
+
+
+public void verifyPaginationIcons() 
+{
+	Assert.assertEquals(true, firstpage.isDisplayed());
+	Assert.assertEquals(true, previous.isDisplayed());
+	Assert.assertEquals(true, nextpage.isDisplayed());
+	Assert.assertEquals(true, lastpage.isDisplayed());
+	
+}
+
+public void verifyFiveStaffdata() {
+	// TODO Auto-generated method stub
+	List<WebElement> staffList = driver.findElements(By.xpath("//*[@class=\"mat-row cdk-row ng-star-inserted\"]"));
+	
+	int staffdata=staffList.size();
+	Assert.assertEquals(staffList.size(), 5);
+	if(staffdata==5)
+	{
+		if(nextpage.isEnabled())
+		{
+		nextpage.click();
+		List<WebElement> staffListnextpage = driver.findElements(By.xpath("//*[@class=\"mat-row cdk-row ng-star-inserted\"]"));
+		Assert.assertEquals(staffListnextpage.size(), 5);
+		}
+		
+		if(previous.isEnabled())
+		{
+		nextpage.click();
+		List<WebElement> staffListnextpage = driver.findElements(By.xpath("//*[@class=\"mat-row cdk-row ng-star-inserted\"]"));
+		Assert.assertEquals(staffListnextpage.size(), 5);
+		}
+		
+		if(firstpage.isEnabled())
+		{
+		nextpage.click();
+		List<WebElement> staffListnextpage = driver.findElements(By.xpath("//*[@class=\"mat-row cdk-row ng-star-inserted\"]"));
+		//int staffdata=staffList.size();
+		Assert.assertEquals(staffListnextpage.size(), 5);
+		}
+	}
+}
+public void verifyPreviouspageDisable() {
+	Assert.assertEquals(previous.isEnabled(), false);
+	
+}
+public void verifyFirstpageDisable() {
+	Assert.assertEquals(firstpage.isEnabled(), false);
+	
+}
+
+
+  }
+
+
+
+	
+
+
+	
+
--- src/test/java/stepDefinitions/HomeStep.java
+++ src/test/java/stepDefinitions/HomeStep.java
@@ -1,5 +1,163 @@
 package stepDefinitions;
 
-public class HomeStep {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import driverFactory.DriverFactory;
+import pageObjectRepository.HomePage;
+
+public class HomeStep 
+{
+	HomePage hp=new HomePage(DriverFactory.getDriver());
+	//WebDriver driver=new ChromeDriver();
+	@Given("Admin is on login Page")
+	public void admin_is_on_login_page() 
+	{
+		hp.landOnloginpage();   
+	    
+	}
+
+	@When("Admin enter valid data in all field and clicks login button")
+	public void admin_enter_valid_data_in_all_field_and_clicks_login_button() 
+	{
+  hp.login();
+	      
+	}
+
+	@Then("Admin should see LMS -Learning management system as title")
+	public void admin_should_see_lms_learning_management_system_as_title() 
+	{
+		
+		hp.validateLmsTitle();
+	      
+	}
+
+	@Then("LMS title should be on the top left corner of page")
+	public void lms_title_should_be_on_the_top_left_corner_of_page() 
+	{
+   hp.verifyTitlePosition();
+	      
+	}
+
+	@Then("Admin should see correct spelling in navigation bar text")
+	public void admin_should_see_correct_spelling_in_navigation_bar_text() 
+	{
+   hp.verifyNevigationbarTextSpelling();
+	      
+	}
+
+	@Then("Admin should see correct spelling and space in LMS title")
+	public void admin_should_see_correct_spelling_and_space_in_lms_title() {
+  
+	      hp.veryfyTitleSpelling();
+	}
+
+	@Then("Admin should see the navigation bar text on the top right side")
+	public void admin_should_see_the_navigation_bar_text_on_the_top_right_side() {
+  hp.check_AlignmentOfTextField();
+	      
+	}
+
+	@Then("Admin should see home in the 1st place")
+	public void admin_should_see_home_in_the_1st_place() throws InterruptedException {
+		hp.checkTextPosition("Home");
+	      
+	}
+
+	@Then("Admin should see program in the 2nd place")
+	public void admin_should_see_program_in_the_2nd_place() throws InterruptedException {
+		hp.checkTextPosition("Program");
+	      
+	}
+
+	@Then("Admin should see batch in the 3rd place")
+	public void admin_should_see_batch_in_the_3rd_place() throws InterruptedException {
+		hp.checkTextPosition("Batch");
+	      
+	}
+
+	@Then("Admin should see class in the 4th place")
+	public void admin_should_see_class_in_the_4th_place() throws InterruptedException {
+		hp.checkTextPosition("Class");
+	      
+	}
+
+	@Then("Admin should see logout in the 5th place")
+	public void admin_should_see_logout_in_the_5th_place() throws InterruptedException {
+		hp.checkTextPosition("Logout");
+	      
+	}
+
+	@Then("Admin should see piechart")
+	public void admin_should_see_piechart() 
+	{
+    hp.pieChartisAvailable();
+	      
+	}
+
+	@Then("Admin should see welcome message with user name and role")
+	public void admin_should_see_welcome_message_with_user_name_and_role() 
+	{
+    hp.verifyEelcomeMessageAndRole();
+	      
+	}
+
+	@Then("Admin should see bar chart for Active and inactive user")
+	public void admin_should_see_bar_chart_for_active_and_inactive_user() {
+  
+		hp.barChartisAvailable();
+	}
+
+	@Then("Admin should see user count Note : count includes Active and inactive")
+	public void admin_should_see_user_count_note_count_includes_active_and_inactive() 
+	{
+  
+	      hp.count("User");
+	}
+
+	@Then("Admin should see staff count Note : count includes Active and inactive")
+	public void admin_should_see_staff_count_note_count_includes_active_and_inactive() {
+		 hp.count("Staff");
+	      
+	}
+
+	@Then("Admin should see Program count Note : count includes Active and inactive")
+	public void admin_should_see_program_count_note_count_includes_active_and_inactive() {
+		hp.count("Programs");
+	      
+	}
+
+	@Then("Admin should see batch couNote : count includes Active and inactive")
+	public void admin_should_see_batch_cou_note_count_includes_active_and_inactive() {
+		 hp.count("Batches");
+	      
+	}
+
+	@Then("Admin should see staff table with pagination icons")
+	public void admin_should_see_staff_table_with_pagination_icons() 
+	{
+	hp.verifyPaginationIcons();
+	      
+	}
+
+	@Then("Admin should see {int} staff data in a page")
+	public void admin_should_see_staff_data_in_a_page(Integer int1) {
+  hp.verifyFiveStaffdata();
+	      
+	}
+
+	@Then("admin should see previous page icon disabled")
+	public void admin_should_see_previous_page_icon_disabled() {
+		 hp.verifyPreviouspageDisable();
+	      
+	}
+
+	@Then("admin should see first page icon disabled")
+	public void admin_should_see_first_page_icon_disabled() {
+  
+		hp.verifyFirstpageDisable();
+	}
 }
